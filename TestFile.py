580ef53cbec2671616d83abbecb4a37b27afbf6a
tmwnwes <tmwatson@wesleyan.edu>
2024-12-08 17:07:09 -0500

    Add files via upload

--- TestFile.py
+++ TestFile.py
@@ -33,6 +33,8 @@ range2_min, range2_max = 0, 480
 # Initialize variables
 counter1 = 50
 counter2 = 50
+clk1LastState = 0
+clk2LastState = 0
 
 # GPIO setup
 GPIO.setmode(GPIO.BCM)
@@ -41,58 +43,56 @@ GPIO.setup(dt1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(clk2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(dt2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# Initialize variables
-counter1 = 50
-counter2 = 50
-
 # **Initialize clk1LastState and clk2LastState with their respective GPIO input states**
 clk1LastState = GPIO.input(clk1)  # Initialize to the current state of clk1
 clk2LastState = GPIO.input(clk2)  # Initialize to the current state of clk2
 
 # Mapping function
 def map_value(value, in_min, in_max, out_min, out_max):
+    #value = max(in_min, min(in_max, value))  # Clamp the value     #maybe turn this on
     return (value - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
 
 # Callback function for Encoder 1
 def rotary_callback1(channel):
-    global counter1, clk1LastState  # Added clk1LastState to track properly
+    global counter1
+
     clkState = GPIO.input(clk1)
     dtState = GPIO.input(dt1)
 
-    if clkState != clk1LastState:  # State has changed
+    if clkState != clk1LastState:
         if dtState != clkState:
             counter1 += 1
         else:
             counter1 -= 1
+
         # Map counter1 to range
-        mapped_value1 = map_value(counter1, 0, 100, range1_min, range1_max)  # Adjusted for example range
+        mapped_value1 = map_value(counter1, 0, 100, range1_min, range1_max)  # Example counter range: 0-100
         print(f"Encoder 1 Counter: {counter1}, Mapped Value: {mapped_value1}")
-    
-    # **Update clk1LastState**
-    clk1LastState = clkState
+
+    clk1LastState = clkState #added this
 
 # Callback function for Encoder 2
 def rotary_callback2(channel):
-    global counter2, clk2LastState  # Added clk2LastState to track properly
+    global counter2
+
     clkState = GPIO.input(clk2)
     dtState = GPIO.input(dt2)
 
-    if clkState != clk2LastState:  # State has changed
+    if clkState != clk2LastState:
         if dtState != clkState:
             counter2 += 1
         else:
             counter2 -= 1
+
         # Map counter2 to range
-        mapped_value2 = map_value(counter2, 0, 100, range2_min, range2_max)  # Adjusted for example range
+        mapped_value2 = map_value(counter2, 0, 100, range2_min, range2_max)  # Example counter range: 0-100
         print(f"Encoder 2 Counter: {counter2}, Mapped Value: {mapped_value2}")
-    
-    # **Update clk2LastState**
-    clk2LastState = clkState
 
-# **Increased bouncetime for better signal stability**
-GPIO.add_event_detect(clk1, GPIO.BOTH, callback=rotary_callback1, bouncetime=5)
-GPIO.add_event_detect(clk2, GPIO.BOTH, callback=rotary_callback2, bouncetime=5)
+    clk2LastState = clkState #added this
 
+# Setup interrupts for both encoders
+GPIO.add_event_detect(clk1, GPIO.BOTH, callback=rotary_callback1, bouncetime=1)
+GPIO.add_event_detect(clk2, GPIO.BOTH, callback=rotary_callback2, bouncetime=1)
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -127,27 +127,6 @@ try:
     draw_Himage.rectangle((80, 50, 130, 100), fill = 0)
     draw_Himage.chord((200, 50, 250, 100), 0, 360, fill = 0)
     epd.display(epd.getbuffer(Himage),epd.getbuffer(Other))
-    time.sleep(2)
-
-    
-    # Drawing on the Vertical image
-    logging.info("2.Drawing on the Vertical image...")
-    Limage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    Limage_Other = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    draw_Himage = ImageDraw.Draw(Limage)
-    draw_Himage_Other = ImageDraw.Draw(Limage_Other)
-    draw_Himage.text((2, 0), 'hello world', font = font18, fill = 0)
-    draw_Himage.text((2, 20), '7.5inch epd', font = font18, fill = 0)
-    draw_Himage_Other.text((20, 50), u'微雪电子', font = font18, fill = 0)
-    draw_Himage_Other.line((10, 90, 60, 140), fill = 0)
-    draw_Himage_Other.line((60, 90, 10, 140), fill = 0)
-    draw_Himage_Other.rectangle((10, 90, 60, 140), outline = 0)
-    draw_Himage_Other.line((95, 90, 95, 140), fill = 0)
-    draw_Himage.line((70, 115, 120, 115), fill = 0)
-    draw_Himage.arc((70, 90, 120, 140), 0, 360, fill = 0)
-    draw_Himage.rectangle((10, 150, 60, 200), fill = 0)
-    draw_Himage.chord((70, 150, 120, 200), 0, 360, fill = 0)
-    epd.display(epd.getbuffer(Limage), epd.getbuffer(Limage_Other))
     time.sleep(2)
 
     logging.info("3.read bmp file")
