8c681fe3ceeb0e739194ffbfd1aad874ee55961a
tmwnwes <tmwatson@wesleyan.edu>
2024-12-08 14:56:11 -0500

    Add files via upload

--- rotary_encoder.py
+++ rotary_encoder.py
@@ -1,69 +1,65 @@
-# from RPi import GPIO
-# from time import sleep
-
-# clk = 19
-# dt = 16
-
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-# GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-# counter = 0
-# clkLastState = GPIO.input(clk)
-
-# try:
-
-#         while True:
-#                 clkState = GPIO.input(clk)
-#                 dtState = GPIO.input(dt)
-#                 if clkState != clkLastState:
-#                         if dtState != clkState:
-#                                 counter += 1
-#                         else:
-#                                 counter -= 1
-#                         print (counter)
-#                 clkLastState = clkState
-#                 sleep(0.01)
-# finally:
-#         GPIO.cleanup()
-
 from RPi import GPIO
 from time import sleep
 
-# Pin definitions
-clk = 19
-dt = 16
+# Pin definitions for Encoder 1
+clk1 = 19
+dt1 = 16
+
+# Pin definitions for Encoder 2
+clk2 = 20
+dt2 = 26
 
 # Initialize variables
-counter = 0
-clkLastState = 0
+counter1 = 0
+counter2 = 0
+clk1LastState = 0
+clk2LastState = 0
 
 # GPIO setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(clk1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(dt1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(clk2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(dt2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# Callback function to handle encoder changes
-def rotary_callback(channel):
-    global counter, clkLastState
+# Callback function for Encoder 1
+def rotary_callback1(channel):
+    global counter1, clk1LastState
 
-    clkState = GPIO.input(clk)
-    dtState = GPIO.input(dt)
+    clkState = GPIO.input(clk1)
+    dtState = GPIO.input(dt1)
 
-    if clkState != clkLastState:
+    if clkState != clk1LastState:
         if dtState != clkState:
-            counter += 1
+            counter1 += 1
         else:
-            counter -= 1
-        print(f"Counter: {counter}")
+            counter1 -= 1
+        print(f"Encoder 1 Counter: {counter1}")
 
-    clkLastState = clkState
+    clk1LastState = clkState
 
-# Setup interrupt for the CLK pin
-GPIO.add_event_detect(clk, GPIO.BOTH, callback=rotary_callback, bouncetime=1)
+# Callback function for Encoder 2
+def rotary_callback2(channel):
+    global counter2, clk2LastState
+
+    clkState = GPIO.input(clk2)
+    dtState = GPIO.input(dt2)
+
+    if clkState != clk2LastState:
+        if dtState != clkState:
+            counter2 += 1
+        else:
+            counter2 -= 1
+        print(f"Encoder 2 Counter: {counter2}")
+
+    clk2LastState = clkState
+
+# Setup interrupts for both encoders
+GPIO.add_event_detect(clk1, GPIO.BOTH, callback=rotary_callback1, bouncetime=1)
+GPIO.add_event_detect(clk2, GPIO.BOTH, callback=rotary_callback2, bouncetime=1)
 
 try:
-    print("Rotary Encoder Test - Press Ctrl+C to exit")
+    print("Dual Rotary Encoder Test - Press Ctrl+C to exit")
     while True:
         sleep(0.1)  # Main loop can remain lightweight
 finally:
